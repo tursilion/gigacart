bfce2601f8d99daba9bd38c4e7ae5647a2d403ff
tursilion <darkmane@harmlesslion.com>
2018-12-15 05:05:00 -0800

    -added new slides for attract and diagnostics -updated attract mode -added diagnostics to main -made main able to change banks on define -added placeholder for keyboard version -added diagnostics bank and code (mostly there) -updated GPL code to launch keyboard or joystick version -also updated GPL code to use asm to bank, to ensure a 16-bit write

--- code/updategfx.cmd
+++ code/updategfx.cmd
@@ -16,22 +16,38 @@ d:\work\ti\videobitmap2border\debug\videobitmap2border CONFIGNOHINTSHARDENHANCED
 d:\work\ti\videobitmap2border\debug\videobitmap2border F18A F18A.bin CONTENT
 d:\work\ti\videobitmap2border\debug\videobitmap2border HLSPLASH HLSplash.bin CONTENT
 d:\work\ti\videobitmap2border\debug\videobitmap2border HLTITLE HLTitle.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border INSTRUCTION1JOY Instruction1Joy.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border INSTRUCTION1KEY Instruction1Key.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border INSTRUCTION2JOY Instruction2Joy.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border INSTRUCTION2KEY Instruction2Key.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border GAMEOVER GameOver.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border COLORBARS ColorBars.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG1 Diag1.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG2 Diag2.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG3 Diag3.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG4 Diag4.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG5 Diag5.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG6 Diag6.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG7 Diag7.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG8 Diag8.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG9 Diag9.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border DIAG14 Diag14.bin CONTENT
+d:\work\ti\videobitmap2border\debug\videobitmap2border ALPHALOCKUP AlphaLockUp.bin CONTENT
 
 @rem prepare the video file
-@rem first -8192 removes the code, second removes the first frame of video to keep the alignment
+@rem first -8192 removes the code, then removes the first three frames of video to keep the alignment (32768 total)
 cd ..
 D:\work\ti\video\tools\cartrepack.exe output.bin output_Cart8.bin
-d:\work\setbinsize\release\setbinsize output_Cart8.bin -8192
-d:\work\setbinsize\release\setbinsize output_Cart8.bin -8192
-d:\work\setbinsize\release\setbinsize output_Cart8.bin 88588288
+d:\work\setbinsize\release\setbinsize output_Cart8.bin -32768
+d:\work\setbinsize\release\setbinsize output_Cart8.bin 88571904
 d:\work\ti\uncartify\debug\uncartify output_Cart8.bin
 
 @rem append the still frames to the video file (note they are not in video format!)
 @rem note we are assuming that the still frames are each exactly 8k and not padding individually
-@rem final size is 128MB minus 8192 for the program and 256 for the GPL (so not based on size of data!)
+@rem final size is 128MB minus 8192 * 3 for the program and 256 for the GPL (so not based on size of data!)
 cd dl_pics
-copy /y /b ..\output_Cart8.bin + /b BorderOut.bin + /b ConfigHintsEasyArcade.bin + /b ConfigHintsEasyEnhanced.bin + /b ConfigHintsHardArcade.bin + /b ConfigHintsHardEnhanced.bin + /b ConfigNoHintsEasyArcade.bin + /b ConfigNoHintsEasyEnhanced.bin + /b ConfigNoHintsHardArcade.bin + /b ConfigNoHintsHardEnhanced.bin + /B F18A.bin + /b HLSplash.bin + /b HLTitle.bin + /b ..\code\sprites.bin /b CartROMData.bin
-d:\work\setbinsize\release\setbinsize CartROMData.bin 134209280
+copy /y /b ..\output_Cart8.bin + /b BorderOut.bin + /b ConfigHintsEasyArcade.bin + /b ConfigHintsEasyEnhanced.bin + /b ConfigHintsHardArcade.bin + /b ConfigHintsHardEnhanced.bin + /b ConfigNoHintsEasyArcade.bin + /b ConfigNoHintsEasyEnhanced.bin + /b ConfigNoHintsHardArcade.bin + /b ConfigNoHintsHardEnhanced.bin + /B F18A.bin + /b HLSplash.bin + /b HLTitle.bin + /b ..\code\sprites.bin + /b Instruction1Joy.bin + /b Instruction1Key.bin + /b Instruction2Joy.bin + /b Instruction2Key.bin + /b GameOver.bin + /b ColorBars.bin + /b Diag1.bin + /b Diag2.bin + /b Diag3.bin + /b Diag4.bin + /b Diag5.bin + /b Diag6.bin + /b Diag7.bin + /b Diag8.bin + /b Diag9.bin + /b Diag14.bin + /b AlphaLockUp.bin /b CartROMData.bin
+d:\work\setbinsize\release\setbinsize CartROMData.bin 134192896
 
 @rem rebuild the code
 cd ..\code
@@ -39,11 +55,22 @@ python \work\f18a\f18a_994a_isu_updater_1.9_beta\tools\xdt99\xas99.py -R Dragons
 @if errorlevel 1 goto error
 d:\work\ti\tiobj2bin\release\tiobj2bin d:\classic99\dsk1\DragonsLair.obj DragonsLairC.bin -raw -block
 d:\work\setbinsize\release\setbinsize DragonsLairC.bin 8192
+@rem keyboard version
+python \work\f18a\f18a_994a_isu_updater_1.9_beta\tools\xdt99\xas99.py -R DragonsLairKey.a99 -L DragonsLairKey.lst -o D:\classic99\dsk1\DragonsLairKey.obj
+@if errorlevel 1 goto error
+d:\work\ti\tiobj2bin\release\tiobj2bin d:\classic99\dsk1\DragonsLairKey.obj DragonsLairKeyC.bin -raw -block
+d:\work\setbinsize\release\setbinsize DragonsLairKeyC.bin 8192
+@rem Diagnostics
+python \work\f18a\f18a_994a_isu_updater_1.9_beta\tools\xdt99\xas99.py -R Dragonostics.a99 -L Dragonostics.lst -o D:\classic99\dsk1\Dragonostics.obj
+@if errorlevel 1 goto error
+d:\work\ti\tiobj2bin\release\tiobj2bin d:\classic99\dsk1\Dragonostics.obj DragonosticsC.bin -raw -block
+d:\work\setbinsize\release\setbinsize DragonosticsC.bin 8192
+
 
 @rem pad up to size and add the GPL part for boot
 copy /y gpl.bin gpl.tmp
 d:\work\setbinsize\release\setbinsize gpl.tmp 256
-copy /y /b DragonsLairC.bin + /b ..\dl_pics\CartROMData.bin + /b gpl.tmp /b Test8.bin
+copy /y /b DragonsLairC.bin + /b DragonsLairKeyC.bin + /b DragonosticsC.bin + /b ..\dl_pics\CartROMData.bin + /b gpl.tmp /b Test8.bin
 goto :EOF
 
 :error
